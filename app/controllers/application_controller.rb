acd5f1c0d854ae99e0228d9cc34a4bfdb1c4a5d4
kokoyuyu1222 <yk.rk1222@gmail.com>
2020-03-19 11:49:19 +0000

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_sign_up_params, if: :devise_controller?
+  protect_from_forgery with: :exception
+
   private
+  def current_cart
+
+    Cart.find(session[:cart_id])
+
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+  end
 
   def after_sign_up_path_for(resource)
   	case resource
