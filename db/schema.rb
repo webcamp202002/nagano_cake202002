a1e309c8efeb875bc157ac9c464af72ef785f0b7
kokoyuyu1222 <yk.rk1222@gmail.com>
2020-03-24 07:02:59 +0000

    購入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_075649) do
+ActiveRecord::Schema.define(version: 2020_03_24_070124) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_075649) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order_id"
   end
 
   create_table "orders", force: :cascade do |t|
