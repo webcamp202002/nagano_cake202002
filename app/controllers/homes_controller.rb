285c13feb74bde93d028e44724b56a7850211cd7
kokoyuyu1222 <yk.rk1222@gmail.com>
2020-03-26 11:00:48 +0000

    トップ

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,8 +2,7 @@ class HomesController < ApplicationController
 	layout 'member'
 	def top
 		@genres =Genre.where(status: "draft")
-		@product = Product.all
-		@products = @product.order("RANDUM()").limit(4)
+		@products = Product.all
 
 	end
 
