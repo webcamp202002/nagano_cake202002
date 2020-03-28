fdaa10d23962e8300f6ff8c9297476c5cc1c3073
webcamp202002 <takoyaki.boys2020@gmail.com>
2020-03-28 08:05:46 +0000

    f

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_113545) do
+ActiveRecord::Schema.define(version: 2020_03_24_070124) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
